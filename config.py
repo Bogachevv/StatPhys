7d3ace934e0ca3bbea654be0213f625f4b964109
Bogachevv <dog67688@gmail.com>
2023-12-03 00:37:58 +0300

    Fix config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import configloader
 import py_singleton
+import json
 
 
 @py_singleton.singleton
@@ -16,23 +17,39 @@ class ConfigLoader(object):
     def __getitem__(self, item):
         return self._loader[item]
 
-    def set(self, key: str | tuple[str], value, check_key: bool = True):
+    def set(self, key: str | tuple, value):
         """
         Change record with key in config.
         It's not implemented by __setitem__ for config safety
         :param key: If key is str then changing cfg[key].
         If key is tuple (key_1, ..., key_n) then changing cfg[key_1][...][key_n]
         :param value: New value
-        :param check_key: If true permit creating new elements
         """
 
-        if check_key and not (key in self._loader):
-            raise ValueError('check_key set to True, but key not in config keys')
-
         if isinstance(key, str):
+            if key not in self._loader:
+                raise ValueError('key not in config keys')
             self._loader[key] = value
         else:
             to_update = self._loader
             for k in key[:-1]:
+                if k not in to_update:
+                    raise ValueError('key not in config keys')
                 to_update = to_update[k]
+
+            if key[-1] not in to_update:
+                raise ValueError('key not in config keys')
             to_update[key[-1]] = value
+
+        with open(self._path, "w", encoding="utf-8") as f:
+            json.dump(obj=dict(self._loader), fp=f, ensure_ascii=False, indent=2, separators=(',', ': '))
+
+
+def main():
+    loader = ConfigLoader()
+    loader.set(('param_initial', 'Температура'), 'val')
+    # print(json.dumps(dict(loader._loader)))
+
+
+if __name__ == '__main__':
+    main()

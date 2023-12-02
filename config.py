3926707bb7dc053c988e965b6499c721422e5ffc
Bogachevv <dog67688@gmail.com>
2023-12-03 00:17:00 +0300

    Implement language.py Modify config.py

--- config.py
+++ config.py
@@ -15,3 +15,24 @@ class ConfigLoader(object):
 
     def __getitem__(self, item):
         return self._loader[item]
+
+    def set(self, key: str | tuple[str], value, check_key: bool = True):
+        """
+        Change record with key in config.
+        It's not implemented by __setitem__ for config safety
+        :param key: If key is str then changing cfg[key].
+        If key is tuple (key_1, ..., key_n) then changing cfg[key_1][...][key_n]
+        :param value: New value
+        :param check_key: If true permit creating new elements
+        """
+
+        if check_key and not (key in self._loader):
+            raise ValueError('check_key set to True, but key not in config keys')
+
+        if isinstance(key, str):
+            self._loader[key] = value
+        else:
+            to_update = self._loader
+            for k in key[:-1]:
+                to_update = to_update[k]
+            to_update[key[-1]] = value
